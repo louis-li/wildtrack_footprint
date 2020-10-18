ec266b9a873902d90c3256d9dab1ef2fa4c99fed
eddie salinas <eddieasalinas@gmail.com>
2020-10-18 14:52:44 -0500

    code for batch processing with 3 models

--- demo_notebook/second_activity.kt
+++ demo_notebook/second_activity.kt
@@ -1,40 +1,175 @@
 package com.example.android.plantsapp
 
 import android.annotation.SuppressLint
-import android.os.Bundle
-import android.widget.Toast
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.ActivityInfo
-import android.graphics.*
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.os.Build
+import android.os.Bundle
 import android.provider.MediaStore
-import androidx.annotation.RequiresApi
-import androidx.appcompat.app.AppCompatActivity;
+import android.text.TextUtils
 import android.view.Gravity
-import android.graphics.Matrix;
-import android.widget.Button
-import android.widget.TextView
-import kotlinx.android.synthetic.main.second_activity.*;
-import java.io.IOException
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.second_activity.*
+import java.io.*
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLEncoder
 import java.time.Instant
 import java.time.format.DateTimeFormatter
-import java.util.*
+
 
 class second_activity  : AppCompatActivity() {
     private lateinit var mClassifier: Classifier
-    private lateinit var mFaceRegressor: EddieFaceRegressor
+    private lateinit var species_classifier:SpeciesClassifier
     private lateinit var mBitmap: Bitmap
-    private lateinit var faceBitmap: Bitmap
+    private lateinit var myVotes:ArrayList<String>
 
     private val mCameraRequestCode = 0
     private val mGalleryRequestCode = 2
 
     private val mInputSize = 299
     private val mModelPath = "output_model.tflite"
-    private val faceModelPath="FaceModelFinal.tflite"
+    private val sModelPath="xception_tf2.tflite"
+    private val sLabelPath="exception_labels.ready.txt"
+    private val sInputSize=224
     private val mLabelPath = "plant_labels.txt"
     private val mSamplePath = "image1.png"
+
+
+    //models for list
+    private lateinit var exception_tflite_Louis:SpeciesClassifier
+    private lateinit var inception_tflite_Louis:SpeciesClassifier
+    private lateinit var nasnet_tflite_louis:SpeciesClassifier
+    private lateinit var model_list:ArrayList<SpeciesClassifier>
+
+
+    private fun sendPredictionResult(p_url: String, kv_pairs: HashMap<String, String>)
+    {
+
+        try {
+            var url=p_url
+            if(kv_pairs.isNotEmpty())
+                {
+                url+="?"
+                var pairs= mutableListOf<String>()
+                //val mutableList = mutableListOf<Kolory>()
+                //val mutableList : MutableList<Kolory> = arrayListOf()
+                for((k,v) in kv_pairs)
+                    {
+                    val new_pair=k+"="+URLEncoder.encode(v,"UTF-8")
+                    //System.out.println("new pair is "+new_pair)
+                    pairs.add(new_pair)
+                    }
+                var req_kvp=TextUtils.join("&",pairs)
+                url+=req_kvp
+                }
+            System.out.println("Final URL for submission is "+url)
+            readURLAsText(url)
+
+            }
+        catch (e: Exception)
+        {
+            e.printStackTrace()
+        }
+    }
+
+
+    //https://stackoverflow.com/questions/8992964/android-load-from-url-to-bitmap
+    //key is "decodestream"
+    fun getBitmapFromURL(src: String?): Bitmap? {
+        return try {
+            val url = URL(src)
+            val connection = url.openConnection() as HttpURLConnection
+            connection.doInput = true
+            connection.connect()
+            val input: InputStream = connection.inputStream
+            val to_ret=BitmapFactory.decodeStream(input)
+            return to_ret
+        } catch (e: IOException) {
+            // Log exception
+            null
+        }
+    }
+
+
+
+    private fun readURLAsText(urls: String):  ArrayList<String>   {
+            //val url = URL("http://192.168.1.76:8000/img_list.txt")
+            val url = URL(urls)
+            val con: HttpURLConnection = url.openConnection() as HttpURLConnection
+            val myisreader=InputStreamReader(con.inputStream)
+            val mybufferedreader=BufferedReader(myisreader)
+            var inputLine=mybufferedreader.readLine()
+            val myLines=ArrayList<String>()
+            while(inputLine!=null)
+                {
+                myLines.add(inputLine)
+                inputLine=mybufferedreader.readLine()
+                }
+            myisreader.close()
+            return myLines
+        }
+
+    fun launchThread() {
+        Thread(Runnable {
+            // a potentially time consuming task
+            val run_id=DateTimeFormatter.ISO_INSTANT.format(Instant.now())
+            System.out.println("A Thread is started")
+            val url_base = "http://192.168.1.76:8000/"
+            var img_fof_url = url_base + "img_list_louis_test.txt"
+            var list_of_files = readURLAsText(img_fof_url)
+            //System.out.println("List : "+list_of_files.toString())
+            //val myurl_encoder=URLEncoder()
+            System.out.println("Downloaded a list of " + list_of_files.size + " files!")
+            for (f in 0 until list_of_files.size) {
+            //for (f in 0 until 10) {
+                //for (f in 0 until 5) {
+                System.out.println("File " + (f+1) + " of "+list_of_files.size+": " + list_of_files[f])
+                val img_url = url_base + list_of_files[f]
+                System.out.println("Image URL is " + img_url)
+                var encoded_img_path = list_of_files[f]
+                //var encoded_img_path=URLEncoder.encode(list_of_files[f],"UTF-8")
+                //encoded_img_path=encoded_img_path.replace("%2F","/")
+
+                val img_url_encoded = url_base + encoded_img_path
+                System.out.println("Encoded Image URL is " + img_url_encoded)
+                val tempBitmap = getBitmapFromURL(img_url_encoded)
+                if (tempBitmap != null) {
+                    for (m in model_list) {
+                        val beforeTime = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
+                        val temp_result = m.recognizeImage(tempBitmap)
+                        val afterTime = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
+                        System.out.println("From url " + img_url + ", model " + m.getName() + " result is " + temp_result)
+                        System.out.println("Times before/after prediction : " + beforeTime + "/" + afterTime)
+                        var infoMap=HashMap<String,String>()
+                        infoMap.put("run_id",run_id)
+                        infoMap.put("file",list_of_files[f])
+                        infoMap.put("file_id",""+f+"")
+                        infoMap.put("result",temp_result)
+                        infoMap.put("model_name",m.getName())
+                        infoMap.put("start_time",beforeTime)
+                        infoMap.put("end_time",afterTime)
+                        val send_req_url="http://192.168.1.76:5000/report_image"
+                        sendPredictionResult(send_req_url,infoMap)
+
+                    }
+                } else {
+                    System.out.println("Bitmap from " + img_url_encoded + " was null!")
+                }
+
+            }
+
+
+        }).start()
+    }
+
+
 
 
     @RequiresApi(Build.VERSION_CODES.O)
@@ -42,70 +177,95 @@ class second_activity  : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         setContentView(R.layout.second_activity)
-        println("mModelPath: "+mModelPath)
-        println("mLabelPath: "+mLabelPath)
-        println("mInputSize: "+mInputSize)
-        println("assets "+assets)
         //mClassifier = Classifier(assets, mModelPath, mLabelPath, mInputSize)
-        //EddieFaceRegressor (assetManager: AssetManager, modelPath: String){
-        mFaceRegressor=EddieFaceRegressor(assets,faceModelPath)
+        //species_classifier=SpeciesClassifier(assets,sModelPath,sLabelPath,sInputSize,"exception_tf2_model")
+        exception_tflite_Louis=SpeciesClassifier(
+            assets,
+            "xception.tflite",
+            "species_13_labels.txt",
+            224,
+            "exception_louis_direct"
+        )
+        inception_tflite_Louis=SpeciesClassifier(
+            assets,
+            "InceptionResNetV2.tflite",
+            "species_13_labels.txt",
+            299,
+            "Inception_louis_direct"
+        )
+        nasnet_tflite_louis=SpeciesClassifier(
+            assets,
+            "NASNetLarge.tflite",
+            "species_13_labels.txt",
+            331,
+            "nasnet_louis_direct"
+        )
+        model_list= ArrayList()
+        myVotes= ArrayList()
+        model_list.add(exception_tflite_Louis)
+        model_list.add(inception_tflite_Louis)
+        model_list.add(nasnet_tflite_louis)
 
-        resources.assets.open(mSamplePath).use {
+
+        //launchThread()
+
+        /*resources.assets.open(mSamplePath).use {
             mBitmap = BitmapFactory.decodeStream(it)
-            //faceBitmap=BitmapFactory.decodeStream(it)
             mBitmap = Bitmap.createScaledBitmap(mBitmap, mInputSize, mInputSize, true)
-            //fa
             mPhotoImageView.setImageBitmap(mBitmap)
-        }
+        }*/
 
         mCameraButton.setOnClickListener {
-            //System.out.println("In mCameraButton.setOnClickListener")
             val callCameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             startActivityForResult(callCameraIntent, mCameraRequestCode)
         }
 
         mGalleryButton.setOnClickListener {
-            //System.out.println("in mGalleryButton.setOnClickListener")
             val callGalleryIntent = Intent(Intent.ACTION_PICK)
             callGalleryIntent.type = "image/*"
             startActivityForResult(callGalleryIntent, mGalleryRequestCode)
         }
         mDetectButton.setOnClickListener {
-            //System.out.println("in mDetectButton.setOnClickListener; Need to call recognizeImage")
-            val beforeTime= DateTimeFormatter.ISO_INSTANT.format(Instant.now())
-            val faceResults=mFaceRegressor.regressImage(mBitmap)
-            val afterTime= DateTimeFormatter.ISO_INSTANT.format(Instant.now())
-            //val results = mClassifier.recognizeImage(mBitmap).firstOrNull()
-            //val results= Array(FloatArray(2))
-            //mResultTextView.text= results?.title+"\n Confidence:"+results?.confidence
-            mResultTextView.text= "TITLE\n Confidence:"
-            System.out.println("Face Results : "+Arrays.toString(faceResults)+"\n")
-            val niceFaceResults=faceResults.forEach { Arrays.toString(it)}
-            val names=listOf("LeftEye","RightEye","Nose")
-            for (x in 0..2){
-                val tempName=names[x]
-                val tempResult=faceResults[x]
-                val x=tempResult[0]
-                val y=tempResult[1]
-                System.out.println(tempName+" : "+x+","+y+"\n")
-            }
-            System.out.println("Time before call to regress "+beforeTime)
-            System.out.println("Time after call to regress "+afterTime)
-            //System.out.println("Face results : "+faceResults[0].toString())
-            var betterBM=addOntoBitMap(faceResults[0][0],faceResults[0][1],mBitmap)
-            betterBM=addOntoBitMap(faceResults[1][0],faceResults[1][1],betterBM)
-            betterBM=addOntoBitMap(faceResults[2][0],faceResults[2][1],betterBM)
-            mPhotoImageView.setImageBitmap(betterBM)
+            var slabel=""
+            myVotes.removeAll(myVotes)
+            for(m in model_list)
+                {
+                    System.out.println("To invoke model " + m.getName())
 
+                    val beforeTime= DateTimeFormatter.ISO_INSTANT.format(Instant.now())
+                    slabel=m.recognizeImage(mBitmap)
+                    val afterTime= DateTimeFormatter.ISO_INSTANT.format(Instant.now())
+                    System.out.println("Time before call " + beforeTime)
+                    System.out.println("Time after call " + afterTime)
+                    myVotes.add(slabel)
+                }
+            System.out.println("Votes are : " + myVotes.toString())
+            slabel=mostFrequent(myVotes)
+            //mResultTextView.text= results?.title+"\n Confidence:"+results?.confidence
+            mResultTextView.text= slabel
         }
 
-        moreInfoText.setOnClickListener{
-            //System.out.println("in moreInfoText.setOnClickListener")
+        /*moreInfoText.setOnClickListener{
             Intent(applicationContext, InfoActivity::class.java).also {
                 startActivity(it)
             }
-        }
+        }*/
     }
+
+
+    private fun mostFrequent(ml: ArrayList<String>):String
+        {
+            if(ml.isEmpty())
+                {
+                return "ERROR, empty list"
+                }
+            //https://stackoverflow.com/questions/48530786/kotlin-find-most-common-element-in-collection
+            val numbersByElement = ml.groupingBy { it }.eachCount()
+            var mostCommon= numbersByElement.maxBy { it.value }!!.key // gives an Int?
+            return mostCommon
+            //gives something like this {1=3, 2=5, 3=4, 5=2, 4=1}
+        }
+
 
     @SuppressLint("MissingSuperCall")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -114,7 +274,11 @@ class second_activity  : AppCompatActivity() {
             if(resultCode == Activity.RESULT_OK && data != null) {
                 mBitmap = data.extras!!.get("data") as Bitmap
                 mBitmap = scaleImage(mBitmap)
-                val toast = Toast.makeText(this, ("Image crop to: w= ${mBitmap.width} h= ${mBitmap.height}"), Toast.LENGTH_LONG)
+                val toast = Toast.makeText(
+                    this,
+                    ("Image crop to: w= ${mBitmap.width} h= ${mBitmap.height}"),
+                    Toast.LENGTH_LONG
+                )
                 toast.setGravity(Gravity.BOTTOM, 0, 20)
                 toast.show()
                 mPhotoImageView.setImageBitmap(mBitmap)
@@ -127,13 +291,16 @@ class second_activity  : AppCompatActivity() {
                 val uri = data.data
 
                 try {
+                    //System.out.println("Setting mbitmap with uri="+uri)
+                    //System.out.println("content resolver "+this.contentResolver)
                     mBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
                 } catch (e: IOException) {
                     e.printStackTrace()
+                    println("Error, see stack trace")
                 }
 
-                println("Success!!!")
-                mBitmap = scaleImage(mBitmap)
+                //println("Just before scaleimage not called!!!")
+                //mBitmap = scaleImage(mBitmap)
                 mPhotoImageView.setImageBitmap(mBitmap)
 
             }
@@ -143,29 +310,6 @@ class second_activity  : AppCompatActivity() {
         }
 
     }
-
-    fun addOntoBitMap(face_x:Float,face_y:Float,tb:Bitmap) : Bitmap
-    {
-        //inverseScale(pred_x:Float,pred_y:Float,b_width:Int,b_height:Int ) : FloatArray
-        val proper_scale_coor=mFaceRegressor.inverseScale(face_x,face_y,tb.width,tb.height)
-        val theCanvas=Canvas(tb)
-        val myPaint =
-            Paint().apply {
-                isAntiAlias = true
-                color = Color.GREEN
-                style = Paint.Style.STROKE
-            }
-        //theCanvas.drawPoint(proper_scale_coor[0],proper_scale_coor[1],myPaint)
-        val proper_x=proper_scale_coor[0].toInt()
-        val proper_y=proper_scale_coor[1].toInt()
-        val rect_dim=5
-        val myRectangle=Rect(proper_x,proper_y,proper_x+rect_dim,proper_y+rect_dim)
-        theCanvas.drawRect(myRectangle,myPaint)
-        val identityMatrix=Matrix()
-        //val resultBitmap=theCanvas.drawBitmap(tb,identityMatrix,null)
-        return Bitmap.createBitmap(tb)
-    }
-
 
     fun scaleImage(bitmap: Bitmap?): Bitmap {
         val orignalWidth = bitmap!!.width
